8c754c2d0f63e19a0db58b325af90949942f596c
Justin Kinsella <justin.d.kinsella@gmail.com>
2019-05-14 00:47:45 -0600

    Finalizeed the column formatting for all the dates. Fished website post deploy

--- script.js
+++ script.js
@@ -39,13 +39,84 @@ window.onload = function() {
             json = await response.json();
             console.log("json: ", json);
 
-            let forecast = "";
-            for (let i=0; i < json.list.length; i++) {
-                forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
-                forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-                forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+            let forecast1 = "";
+            let day = json.list[0].dt_txt.charAt(9);
+            // for (let i=0; i < json.list.length; i++) {
+            //     forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
+            //     forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
+            //     forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+
+                
+            //     if(json.list[i].dt_txt.charAt(9) == day){
+            //         document.getElementById("day1").innerHTML += forecast;
+            //     }
+            //     if(json.list[i].dt_txt.charAt(9) == day + 1){
+            //         document.getElementById("day2").innerHTML += forecast;
+            //     }               
+            //     if(json.list[i].dt_txt.charAt(9) == day + 2){
+            //         document.getElementById("day3").innerHTML += forecast;
+            //     }
+            //     if(json.list[i].dt_txt.charAt(9) == day + 3){
+            //         document.getElementById("day4").innerHTML += forecast;
+            //     }
+            //     if(json.list[i].dt_txt.charAt(9) == day + 4){
+            //         document.getElementById("day5").innerHTML += forecast;
+            //     }
+            // }
+            let i=0;
+            forecast1 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY') + "</h2>";
+            while(json.list[i].dt_txt.charAt(9) == day){
+                forecast1 += "<h3>" + moment(json.list[i].dt_txt).format("h:mm:ss a") + "</h3>";
+                forecast1 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
+                forecast1 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+                i++;
             }
-            document.getElementById("weatherResults").innerHTML += forecast;
+            document.getElementById("day1").innerHTML += forecast1;
+            
+            day++;
+            let forecast2 = "";
+            forecast2 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY') + "</h2>";
+            while(json.list[i].dt_txt.charAt(9) == day){
+                forecast2 += "<h3>" + moment(json.list[i].dt_txt).format("h:mm:ss a") + "</h3>";
+                forecast2 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
+                forecast2 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+                i++;
+            }
+            document.getElementById("day2").innerHTML += forecast2;
+            
+            day++;
+            let forecast3 = "";
+            forecast3 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY') + "</h2>";
+            while(json.list[i].dt_txt.charAt(9) == day){
+                forecast3 += "<h3>" + moment(json.list[i].dt_txt).format("h:mm:ss a") + "</h3>";
+                forecast3 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
+                forecast3 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+                i++;
+            }
+            document.getElementById("day3").innerHTML += forecast3;
+
+            day++;
+            let forecast4 = "";
+            forecast4 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY') + "</h2>";
+            while(json.list[i].dt_txt.charAt(9) == day){
+                forecast4 += "<h3>" + moment(json.list[i].dt_txt).format("h:mm:ss a") + "</h3>";
+                forecast4 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
+                forecast4 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+                i++;
+            }
+            document.getElementById("day4").innerHTML += forecast4;
+            
+            day++;
+            let forecast5 = "";
+            forecast5 += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY') + "</h2>";
+            while(json.list[i].dt_txt.charAt(9) == day){
+                forecast5 += "<h3>" + moment(json.list[i].dt_txt).format("h:mm:ss a") + "</h3>";
+                forecast5 += "<p>Temperature: " + json.list[i].main.temp + "</p>";
+                forecast5 += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+                i++;
+            }
+            document.getElementById("day5").innerHTML += forecast5;
+
 
         }catch(err) {
             console.log(err);
